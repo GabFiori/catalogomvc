5f5f165f01414f3474270c79f7dfe3f4a8476191
Gabriel Fiori <140767553+GabFiori@users.noreply.github.com>
2024-11-23 20:57:48 -0300

    Update faixa.js

--- src/models/faixa.js
+++ src/models/faixa.js
@@ -4,7 +4,6 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Faixa extends Model {
     static associate(models) {
-      // Associando faixa ao disco
       this.belongsTo(models.Disco, {
         foreignKey: 'disco_id',
         as: 'disco',
@@ -14,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
   Faixa.init({
     nome: DataTypes.STRING,
     duracao: DataTypes.INTEGER,
-    disco_id: DataTypes.INTEGER // Chave estrangeira associando a faixa ao disco
+    disco_id: DataTypes.INTEGER 
   }, {
     sequelize,
     modelName: 'Faixa',
