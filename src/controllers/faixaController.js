dccc306a81daf179ce1a942d2c418c62923f7950
Gabriel Fiori <140767553+GabFiori@users.noreply.github.com>
2024-11-24 21:17:32 -0300

    Update faixaController.js

--- src/controllers/faixaController.js
+++ src/controllers/faixaController.js
@@ -1,7 +1,6 @@
 const { Faixa, Disco } = require("../models");
 
 module.exports = {
-  // Criar uma nova faixa
   async create(req, res) {
     try {
       const { nome, duracao, disco_id } = req.body;
@@ -9,23 +8,20 @@ module.exports = {
       if (!nome || !duracao || !disco_id) {
         return res.status(400).json({ error: "Todos os campos são obrigatórios." });
       }
-  
-      // Verificando se o disco existe
+      
       const disco = await Disco.findByPk(disco_id);
       if (!disco) {
         return res.status(404).json({ error: "Disco não encontrado" });
       }
   
-      // Criando a faixa associada ao disco
       const faixa = await Faixa.create({
         nome,
         duracao,
-        disco_id, // Associando a faixa ao disco
+        disco_id, 
       });
   
       console.log("Faixa criada com sucesso, redirecionando para /discos/:id");
   
-      // Redirecionando para a página de detalhes do disco, agora com a faixa adicionada
       return res.redirect(`/discos/${disco_id}`);
     } catch (error) {
       console.error("Erro ao criar faixa:", error);
@@ -33,13 +29,12 @@ module.exports = {
     }
   },
 
-  // Obter todas as faixas
   async getAll(req, res) {
     try {
       const faixas = await Faixa.findAll({
         include: {
           model: Disco,
-          as: 'disco', // Incluir o disco associado à faixa
+          as: 'disco', 
         },
       });
       return res.status(200).json(faixas);
@@ -64,14 +59,13 @@ module.exports = {
         return res.status(404).json({ error: "Disco não encontrado" });
       }
   
-      return res.render("details", { disco }); // Renderizando a página de detalhes
+      return res.render("details", { disco });
     } catch (error) {
       console.error("Erro ao buscar disco:", error);
       return res.status(500).json({ error: "Erro ao buscar disco" });
     }
   },
 
-  // Atualizar uma faixa
   async update(req, res) {
     try {
       const { id } = req.params;
@@ -82,13 +76,11 @@ module.exports = {
         return res.status(404).json({ error: "Faixa não encontrada" });
       }
 
-      // Verificando se o disco existe
       const disco = await Disco.findByPk(disco_id);
       if (!disco) {
         return res.status(404).json({ error: "Disco não encontrado" });
       }
 
-      // Atualizando a faixa
       await faixa.update({
         nome,
         duracao,
@@ -102,7 +94,6 @@ module.exports = {
     }
   },
 
-  // Deletar uma faixa
   async delete(req, res) {
     try {
       const { id } = req.params;
